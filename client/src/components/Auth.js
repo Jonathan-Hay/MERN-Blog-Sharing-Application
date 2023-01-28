e71b3d52ee2df8930425ffc190766291b0f27db5
Jonathan-Hay <jonathanhay08@hotmail.com>
2023-01-28 19:59:55 +0000

    added redux store for logged in state

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import { Button, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useState } from "react";
 
 const Auth = () => {
-    return (
-        <div>
-            <p>Authentication</p>
-        </div>
-    );
+  const [isSignup, setIsSignup] = useState(false);
+
+  return (
+    <React.Fragment>
+      <form>
+        <Box
+          display="flex"
+          flexDirection="column"
+          width={"80%"}
+          margin={"auto"}
+          border={3}
+          borderColor="black"
+          marginTop={5}
+          boxShadow="12px 12px 25px #6b6b6b"
+          padding={3.5}
+          borderRadius={6}
+        >
+          <Typography>Sign up</Typography>
+
+          {isSignup && <TextField margin="normal" placeholder="account name" />}
+
+          <TextField margin="normal" placeholder="email" />
+          <TextField margin="normal" placeholder="password" />
+          <Button variant="outlined" sx={{ borderRadius: 3, marginTop: 3 }}>
+            Sign up
+          </Button>
+        </Box>
+      </form>
+    </React.Fragment>
+  );
 };
 
 export default Auth;
