5bbddd7ee8b2a96f54e96803454e30ddd4e03751
Jonathan-Hay <jonathanhay08@hotmail.com>
2023-01-30 16:10:43 +0000

    add blog functionaly done

--- client/src/components/AddBlogPost.js
+++ client/src/components/AddBlogPost.js
@@ -1,13 +1,17 @@
 import { Box, Button, InputLabel, TextField, Typography } from "@mui/material";
 import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const labelStyles = { mb: 1, mt: 3, fontSize: "20px", fontWeight: "bold" };
 
 const AddBlogPost = () => {
+  const navigate = useNavigate();
+
   const [inputData, setInputData] = useState({
-    title: "aa",
-    imageURL: "i",
-    text: "t",
+    title: "",
+    imageURL: "",
+    text: "",
   });
 
   const inputChangeHandler = (event) => {
@@ -17,13 +21,25 @@ const AddBlogPost = () => {
     }));
   };
 
-  const addPostHandler = () => {
+  const addPostHandler = async () => {
+    const res = await axios
+      .post("http://localhost:5000/post/new", {
+        title: inputData.title,
+        text: inputData.text,
+        image: inputData.imageURL,
+        author: localStorage.getItem("userID"),
+      })
+      .catch((e) => console.log(e));
 
+    const data = await res.data;
+
+    return data;
   };
 
   const submitHandler = (event) => {
-    event.preventDefault();
     console.log(inputData);
+    event.preventDefault();
+    addPostHandler().then(data => console.log(data)).then(() => navigate("/blog-feed"));
   };
 
   return (
@@ -65,11 +81,16 @@ const AddBlogPost = () => {
           >
             Image URL
           </InputLabel>
-          <TextField />
+          <TextField
+            value={inputData.imageURL}
+            onChange={inputChangeHandler}
+            name="imageURL"
+          />
 
           <InputLabel sx={labelStyles}>Text</InputLabel>
           <TextField
             multiline={true}
+            value={inputData.text}
             rows={5}
             margin="auto"
             name="text"
