4c766da37dcbd82e32ba55947307f181af4761dc
Jonathan-Hay <jonathanhay08@hotmail.com>
2023-02-03 17:09:58 +0000

    Upgraded routes to router v6 style. No protection for auth yet

---
+++ client/src/components/RootLayout.js
@@ -0,0 +1,17 @@
+import { Outlet, useNavigation } from 'react-router-dom';
+
+import Header from './Header';
+
+function RootLayout() {
+
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet /> 
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
